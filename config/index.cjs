8362fbe963fc9c7db53644662991d226fb71ec2b
Marchthreeday <80246951+Marchthreeday@users.noreply.github.com>
2023-07-12 12:21:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,9 +13,63 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'cfb1ca67559c811de40fd26756685150',
 
+  IS_SHOW_COLOR: true,
+
   PROVINCE: '河南',
   CITY: '濮阳市',
 
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '猪猪',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: ' ',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'PI0kuvkms3_N469an6I4bTJnX6qdnNBp8lEIunDj3kI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '06-01',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '猪猪阴历生日', year: '1999', date: '04-18',
+        },
+        {
+          type: '节日', name: '在一起纪念日', year: '2022', date: '07-23',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-07-23' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '桐桐',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o5gIU6cQA_9Uvv4lOKQgE7EWgPcY',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'PI0kuvkms3_N469an6I4bTJnX6qdnNBp8lEIunDj3kI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-10',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '猪猪阴历生日', year: '1999', date: '04-18',
+        },
+        {
+          type: '节日', name: '在一起纪念日', year: '2022', date: '07-23',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-07-23' },
+      ],
+    },
+  ],
+
+  // 功能开关,打开：true，关闭：false
   SWITCH: {
     /** 每日天气 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
@@ -27,52 +81,61 @@ const USER_CONFIG = {
 
     /** 每日N句 */
     // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    CIBA: true,
+    CIBA: false,
     
     // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    oneTalk: true,
+    oneTalk: false,
     
     // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     earthyLoveWords: true,
+    
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: false,
   
     /** 生日消息和节日消息 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     birthdayMessage: true,
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },  
+
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: 'b2475209cdfb09f96c347752e6ec4701',
     
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: false,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 1,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 0,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
   },
 
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FYveRk8hIAy4FgKZAv1d1jzYLiR00118t_mIT_LOICI',
-
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1999', date: '04-18',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '*生日', name: '小刘', year: '1999', date: '03-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-07-23' },
-        // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
+  IS_SHOW_COLOR: true,
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  //CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
@@ -85,4 +148,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
